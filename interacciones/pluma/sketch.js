07fe99bd59915b70f901906ffed2614e1b316f9f
maurovillena <maurovillena>
2015-11-23 11:57:58 -0300

    pluma

--- interacciones/pluma/sketch.js
+++ interacciones/pluma/sketch.js
@@ -1,12 +1,12 @@
 var widthControl;
 var message;
-var veces = 20;
+var veces = 1;
 
 function setup() {
-  var canvas = createCanvas(600, 300);
-  canvas.parent('inter002');
-  //createCanvas(800, 600);
-  background(224);
+  //var canvas = createCanvas(600, 300);
+  //canvas.parent('inter002');
+  createCanvas(800, 600);
+  background(255);
   textFont("Merriweather");
   widthControl = createSlider(2, 60, 2);
   widthControl.position(25, 25);
@@ -14,12 +14,19 @@ function setup() {
   textSize(16);
   text("Presiona 'r' para comenzar de nuevo", 10, 25);
   cursor(ARROW);
+  fill(244);
+  rect(m, m, width - m, height - m);
+}
+  var m = 40;
+function insidePage(){
+
+  if (mouseX > m && mouseX < width - m && mouseY > m && mouseY < height - m ){return true;}else{return false;}
 }
 
 function draw(){
-
-  if (mouseIsPressed){
+  if (mouseIsPressed && insidePage()){
   stroke(0);
+  
   strokeWeight(9);
   strokeCap(ROUND);
      for (var i = 0; i < veces; i++){
